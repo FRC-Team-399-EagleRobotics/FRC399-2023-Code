70a6652d7f173a641547eaacc151ec5c3cf40ba8
DriveLaptop <99833632+DarianR785@users.noreply.github.com>
2023-02-22 17:56:31 -0800

    Started putting SwerveDrive in our code
    Still wondering if there should be a command file for swerve drive
    
    Co-Authored-By: Seti <45839707+Gameboyghost@users.noreply.github.com>

--- FRC399-2023-Experimental/src/main/java/frc/robot/RobotContainer.java
+++ FRC399-2023-Experimental/src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.autonomous.AutonomousDrive;
@@ -20,10 +20,26 @@ import frc.robot.autonomous.AutonomousDrive2;
 import frc.robot.autonomous.AutonomousVisionAim;
 import frc.robot.commands.Tankdrive;
 import frc.robot.commands.VisionAimCommand;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.Limelight;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.BalanceCommand;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import java.util.List;
 
 
 /**
@@ -37,14 +53,22 @@ public class RobotContainer {
 
   //public static Joystick driver = new Joystick(1);
   public static Joystick operator = new Joystick(0);
+
   public static Joystick rightJoy = new Joystick(1);
   public static Joystick leftJoy = new Joystick(2);
+
+  Joystick m_driver = new Joystick(1);
+
   //public double stickL = RobotContainer.driver.getRawAxis(1);
   //public double stickR = RobotContainer.driver.getRawAxis(5); 
 
   //----Drivetrain-----
   private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
   private final Tankdrive m_tankdrive = new Tankdrive(m_drivetrainSubsystem, 0, 0);
+
+  // Swerve Drive
+  private final DriveSubsystem m_SwerveDrive = new DriveSubsystem();
+  
   // NavX(gyro)
   private final BalanceCommand m_balance = new BalanceCommand(m_drivetrainSubsystem);
   // Auton commands
@@ -67,6 +91,18 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
 
+        // Configure default commands
+        m_SwerveDrive.setDefaultCommand(
+          // The left stick controls translation of the robot.
+          // Turning is controlled by the X axis of the right stick.
+          new RunCommand(
+              () -> m_SwerveDrive.drive(
+                  -MathUtil.applyDeadband(m_driver.getRawAxis(1), Constants.Swerve.OIConstants.kDriveDeadband),
+                  -MathUtil.applyDeadband(m_driver.getRawAxis(0), Constants.Swerve.OIConstants.kDriveDeadband),
+                  -MathUtil.applyDeadband(m_driver.getRawAxis(2), Constants.Swerve.OIConstants.kDriveDeadband),
+                  true, true),
+                  m_SwerveDrive));
+
   }
 
   /**
@@ -77,6 +113,11 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     m_drivetrainSubsystem.setDefaultCommand(m_tankdrive);
+
+    new JoystickButton(m_driver, Button.kR1.value)
+    .whileTrue(new RunCommand(
+        () -> m_SwerveDrive.setX(),
+        m_SwerveDrive));
 
     //new RepeatCommand(new VisionAimCommand(m_drivetrainSubsystem, limelight));
 
@@ -102,6 +143,47 @@ public class RobotContainer {
     
 
     return drive; // Runs the commands
+
+    /* 
+    // Create config for trajectory
+    TrajectoryConfig config = new TrajectoryConfig(
+        AutoConstants.kMaxSpeedMetersPerSecond,
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(DriveConstants.kDriveKinematics);
+
+    // An example trajectory to follow. All units in meters.
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(3, 0, new Rotation2d(0)),
+        config);
+
+    var thetaController = new ProfiledPIDController(
+        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+        exampleTrajectory,
+        m_robotDrive::getPose, // Functional interface to feed supplier
+        DriveConstants.kDriveKinematics,
+
+        // Position controllers
+        new PIDController(AutoConstants.kPXController, 0, 0),
+        new PIDController(AutoConstants.kPYController, 0, 0),
+        thetaController,
+        m_robotDrive::setModuleStates,
+        m_robotDrive);
+
+    // Reset odometry to the starting pose of the trajectory.
+    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false, false));
+    */
     
   }
 }
